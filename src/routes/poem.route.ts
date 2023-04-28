8c92dc61d2425aa8b4265fcc1ed4e96c595470fc
JustRollingDevs <m.shrief.dev@proton.me>
2023-04-28 22:56:32 +0300

    added validation to poem & poet routes

--- src/routes/poem.route.ts
+++ src/routes/poem.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { param } from 'express-validator';
+import { body, param } from 'express-validator';
 // Controller
 import PoemController from '../controllers/poem.controller';
 // Types
 import { IRoute } from '../interfaces/route.interface';
+// middlewares
 import validate from '../middlewares/validate.middleware';
 
 export default class PoemRoute implements IRoute {
@@ -17,9 +18,36 @@ export default class PoemRoute implements IRoute {
   private initializeRoutes() {
     this.router.get('/poems', this.controller.indexWithPoetName);
     this.router.get('/poems_intros', this.controller.indexIntrosWithPoetName);
-    this.router.get('/poem/:id', this.controller.indexOneWithPoet);
-    this.router.post('/poem/', this.controller.post);
-    this.router.put('/poem/:id', this.controller.update);
-    this.router.delete('/poem/:id', this.controller.remove);
+    this.router.get(
+      '/poem/:id',
+      validate([param('id').optional().notEmpty().isMongoId()]),
+      this.controller.indexOneWithPoet
+    );
+    this.router.post(
+      '/poem/',
+      validate([
+        body('intro').notEmpty().isString().isLength({ max: 50 }),
+        body('poet').isMongoId(),
+        body('verses.*').notEmpty().isString().isLength({ max: 50 }),
+        body('reviewed').optional().notEmpty().isBoolean(),
+      ]),
+      this.controller.post
+    );
+    this.router.put(
+      '/poem/:id',
+      validate([
+        param('id').optional().notEmpty().isMongoId(),
+        body('intro').optional().notEmpty().isString().isLength({ max: 50 }),
+        body('poet').optional().isMongoId(),
+        body('verses.*').optional().notEmpty().isString().isLength({ max: 50 }),
+        body('reviewed').optional().notEmpty().isBoolean(),
+      ]),
+      this.controller.update
+    );
+    this.router.delete(
+      '/poem/:id',
+      validate([param('id').optional().notEmpty().isMongoId()]),
+      this.controller.remove
+    );
   }
 }
--- src/routes/poet.route.ts
+++ src/routes/poet.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { param } from 'express-validator';
+import { body, param } from 'express-validator';
 // Controller
 import PoetController from '../controllers/poet.controller';
 // Types
 import { IRoute } from '../interfaces/route.interface';
+// middlewares
 import validate from '../middlewares/validate.middleware';
 
 export default class PoetRoute implements IRoute {
@@ -16,10 +17,25 @@ export default class PoetRoute implements IRoute {
 
   private initializeRoutes() {
     this.router.get('/poets', this.controller.index);
-    this.router.post('/poet', this.controller.post);
+    this.router.post(
+      '/poet',
+      validate([
+        body('name').notEmpty().isString().isLength({ max: 50 }),
+        body('time_period').isString().isLength({ max: 50 }),
+        body('bio').notEmpty().isString().isLength({ max: 300 }),
+        body('reviewed').optional().notEmpty().isBoolean(),
+      ]),
+      this.controller.post
+    );
     this.router.put(
       '/poet/:id',
-      validate([param('id').notEmpty().isMongoId()]),
+      validate([
+        param('id').optional().notEmpty().isMongoId(),
+        body('name').optional().notEmpty().isString().isLength({ max: 50 }),
+        body('time_period').optional().isString().isLength({ max: 50 }),
+        body('bio').optional().notEmpty().isString().isLength({ max: 300 }),
+        body('reviewed').optional().notEmpty().isBoolean(),
+      ]),
       this.controller.update
     );
     this.router.delete(
