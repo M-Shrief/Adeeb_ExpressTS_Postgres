bcac96d9436de39cb7c88c64f19a5e97308123f2
JustRollingDevs <m.shrief.dev@proton.me>
2023-04-29 10:13:18 +0300

    added validation to ChosenVerse's routes

--- src/routes/chosenVerse.route.ts
+++ src/routes/chosenVerse.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { param } from 'express-validator';
+import { body, param } from 'express-validator';
 // Controller
 import ChosenVerseController from '../controllers/chosenVerse.controller';
 // Types
@@ -17,10 +17,39 @@ export default class ChosenVerseRoute implements IRoute {
 
   private initializeRoutes() {
     this.router.get('/chosenverses', this.controller.indexWithPoetName);
-    this.router.get('/chosenverses/:num', this.controller.indexRandom);
-    this.router.get('/chosenverse/:id', this.controller.indexOneWithPoetName);
-    this.router.post('/chosenverse', this.controller.post);
-    this.router.put('/chosenverse/:id', this.controller.update);
+    this.router.get(
+      '/chosenverses/:num',
+      validate([param('num').isInt()]),
+      this.controller.indexRandom
+    );
+    this.router.get(
+      '/chosenverse/:id',
+      validate([param('id').isMongoId()]),
+      this.controller.indexOneWithPoetName
+    );
+    this.router.post(
+      '/chosenverse',
+      validate([
+        body('poet').isMongoId(),
+        body('poem').isMongoId(),
+        body('tags').isString(),
+        body('verses.*').isString().isLength({ max: 50 }),
+        body('reviewed').optional().isBoolean(),
+      ]),
+      this.controller.post
+    );
+    this.router.put(
+      '/chosenverse/:id',
+      validate([
+        param('id').optional().isMongoId(),
+        body('poet').optional().isMongoId(),
+        body('poem').optional().isMongoId(),
+        body('tags').optional().isString(),
+        body('verses.*').optional().isString().isLength({ max: 50 }),
+        body('reviewed').optional().isBoolean(),
+      ]),
+      this.controller.update
+    );
     this.router.delete('/chosenverse/:id', this.controller.remove);
   }
 }
