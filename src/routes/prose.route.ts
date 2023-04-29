d977087eec9b63323c413a8be9bf5fe865bd2864
JustRollingDevs <m.shrief.dev@proton.me>
2023-04-29 11:44:49 +0300

    added validation for prose's route

--- src/routes/prose.route.ts
+++ src/routes/prose.route.ts
@@ -17,10 +17,41 @@ export default class ProseRoute implements IRoute {
 
   private initalizeRoutes() {
     this.router.get('/proses', this.controller.indexWithPoetName);
-    this.router.get('/proses/:num', this.controller.indexRandom);
-    this.router.get('/prose/:id', this.controller.indexOneWithPoetName);
-    this.router.post('/prose', this.controller.post);
-    this.router.put('/prose/:id', this.controller.update);
-    this.router.delete('/prose/:id', this.controller.remove);
+    this.router.get(
+      '/proses/:num',
+      validate([param('num').isInt()]),
+      this.controller.indexRandom
+    );
+    this.router.get(
+      '/prose/:id',
+      validate([param('id').isMongoId()]),
+      this.controller.indexOneWithPoetName
+    );
+    this.router.post(
+      '/prose',
+      validate([
+        body('poet').isMongoId(),
+        body('tags').notEmpty().isString(),
+        body('qoute').notEmpty().isString().isLength({ max: 300 }),
+        body('reviewed').isBoolean(),
+      ]),
+      this.controller.post
+    );
+    this.router.put(
+      '/prose/:id',
+      validate([
+        param('id').isMongoId(),
+        body('poet').optional().isMongoId(),
+        body('tags').optional().notEmpty().isString(),
+        body('qoute').optional().notEmpty().isString().isLength({ max: 300 }),
+        body('reviewed').optional().isBoolean(),
+      ]),
+      this.controller.update
+    );
+    this.router.delete(
+      '/prose/:id',
+      validate([param('id').isMongoId()]),
+      this.controller.remove
+    );
   }
 }
