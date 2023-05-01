a7cb9cf0c5fcf62a8d6f0aeb3113e939a1476056
JustRollingDevs <m.shrief.dev@proton.me>
2023-05-01 14:48:13 +0300

    removed general cache for GET requests, extended api limiter for 1000 requests

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,6 @@ export default class App {
     this.app.use(compression());
     this.app.use(helmet());
     this.app.use(cookieParser());
-    this.app.use(setCache); // Cache all get Requests for 5 minutes
     this.app.use(
       cors<Request>({
         origin: 'http://localhost:3000',
@@ -58,13 +57,13 @@ export default class App {
   private initializeRoutes(routes: IRoute[]): void {
     const apiLimiter = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 300, // Limit each IP to 300 requests per `window` (here, per 15 minutes)
+      max: 1000, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
       standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
       legacyHeaders: false, // Disable the `X-RateLimit-*` headers
       message: async (req: Request, res: Response) => {
         res
           .status(429) // too many request
-          .send('You can only make 100 requests every 15 minutes.');
+          .send('You can only make 1000 requests every 15 minutes.');
       },
     });
 
