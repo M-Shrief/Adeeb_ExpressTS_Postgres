d39534c01152cfe1c75d2037e8304ad2412915e3
M_Shrief <m.shrief.dev@proton.me>
2023-05-15 19:37:52 +0300

    added API tests for /GET requests in prose component, standarized validation errors on ProseRoute with ERROR_MSG from prose.route

--- src/components/prose/prose.route.ts
+++ src/components/prose/prose.route.ts
@@ -4,6 +4,7 @@ import { body, param, query } from 'express-validator';
 import { ProseController } from './prose.controller';
 // Types
 import { IRoute } from '../../interfaces/route.interface';
+import { ERROR_MSG } from '../../interfaces/prose.interface';
 // middlewares
 import { validate } from '../../middlewares/validate.middleware';
 import { setCache } from '../../middlewares/cache.middleware';
@@ -22,66 +23,60 @@ export class ProseRoute implements IRoute {
       '/proses/random',
       validate([
         // it doesn't give error when num != number
-        query('num').optional().isInt().withMessage('Accepts numbers only'),
+        query('num').optional().isInt().withMessage(ERROR_MSG.NUM),
       ]),
       this.controller.indexRandomWithPoetName,
     );
     this.router.get(
       '/prose/:id',
-      validate([param('id').isMongoId().withMessage('prose not found')]),
+      validate([param('id').isMongoId().withMessage(ERROR_MSG.NOT_FOUND)]),
       this.controller.indexOneWithPoetName,
     );
     this.router.post(
       '/prose',
       validate([
-        body('poet').isMongoId().withMessage('poet not found'),
+        body('poet').isMongoId().withMessage(ERROR_MSG.POET),
 
         body('tags')
           .isLength({ min: 4, max: 50 })
           .isString()
           .escape()
-          .withMessage('tags should be letters, and max 50 letters length'),
+          .withMessage(ERROR_MSG.TAGS),
 
         body('qoute')
           .isLength({ min: 4, max: 400 })
           .isString()
-          .withMessage('qoute should be letters, and max 400 letters length'),
+          .withMessage(ERROR_MSG.QOUTE),
 
-        body('reviewed')
-          .optional()
-          .isBoolean()
-          .withMessage('reviewed must be true or false'),
+        body('reviewed').optional().isBoolean().withMessage(ERROR_MSG.REVIEWED),
       ]),
       this.controller.post,
     );
     this.router.put(
       '/prose/:id',
       validate([
-        param('id').isMongoId().withMessage('prose not found'),
+        param('id').isMongoId().withMessage(ERROR_MSG.NOT_FOUND),
 
-        body('poet').isMongoId().withMessage('poet not found'),
+        body('poet').isMongoId().withMessage(ERROR_MSG.POET),
 
         body('tags')
           .isLength({ min: 4, max: 50 })
           .isString()
           .escape()
-          .withMessage('tags should be letters, and max 50 letters length'),
+          .withMessage(ERROR_MSG.TAGS),
 
         body('qoute')
           .isLength({ min: 4, max: 400 })
           .isString()
-          .withMessage('qoute should be letters, and max 400 letters length'),
+          .withMessage(ERROR_MSG.QOUTE),
 
-        body('reviewed')
-          .optional()
-          .isBoolean()
-          .withMessage('reviewed must be true or false'),
+        body('reviewed').optional().isBoolean().withMessage(ERROR_MSG.REVIEWED),
       ]),
       this.controller.update,
     );
     this.router.delete(
       '/prose/:id',
-      validate([param('id').isMongoId().withMessage('prose not found')]),
+      validate([param('id').isMongoId().withMessage(ERROR_MSG.NOT_FOUND)]),
       this.controller.remove,
     );
   }
--- src/interfaces/prose.interface.ts
+++ src/interfaces/prose.interface.ts
@@ -4,6 +4,13 @@ export enum ERROR_MSG {
   NOT_AVAILABLE = 'No prose available',
   NOT_FOUND = "Prose's not found",
   NOT_VALID = 'Data for prose is not valid',
+  // Query
+  NUM = 'Accepts numbers only',
+  // Inner properties
+  POET = "Poet's not found",
+  TAGS = 'tags should be letters, and max 50 letters length',
+  QOUTE = 'qoute should be letters, and max 400 letters length',
+  REVIEWED = 'reviewed must be true or false',
 }
 
 export interface ProseType {
