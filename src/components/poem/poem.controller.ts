1751f18f0337c921022ca42ebe2f4435e398fbe0
M-Shrief <m.shrief.dev@proton.me>
2023-08-02 11:51:22 +0300

    added E2E test for components/poem

--- src/components/poem/poem.controller.ts
+++ src/components/poem/poem.controller.ts
@@ -102,6 +102,7 @@ export class PoemController {
           ERROR_MSG.NOT_VALID,
           true,
         );
+      // Bug: It gives error when using status()
       res.sendStatus(HttpStatusCode.ACCEPTED).send(poem);
     } catch (error) {
       next(error);
@@ -113,6 +114,7 @@ export class PoemController {
       const poem = await this.poemService.remove(req.params.id);
       if (!poem)
         throw new AppError(HttpStatusCode.NOT_FOUND, ERROR_MSG.NOT_FOUND, true);
+      // Bug: It gives error when using status()
       res.sendStatus(HttpStatusCode.ACCEPTED).send(poem);
     } catch (error) {
       next(error);
