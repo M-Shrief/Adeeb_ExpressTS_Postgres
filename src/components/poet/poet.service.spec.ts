04d88f43f15fe026a73163f4a4d9a714949bf059
M-Shrief <m.shrief.dev@proton.me>
2023-11-21 11:46:07 +0200

    Fully testing PoetService.

--- src/components/poet/poet.service.spec.ts
+++ src/components/poet/poet.service.spec.ts
@@ -2,11 +2,39 @@ import { describe, expect, it, vi, test, beforeAll } from 'vitest'
 // Component
 import {PoetDB, PoetRedis} from './poet.repository'
 import {PoetService} from './poet.service'
-import { Poet } from './poet.entity'
+import { Poet, TimePeriodType } from './poet.entity'
 import { AppDataSource } from '../../db'
+import { UpdateResult } from 'typeorm'
 
 
 describe.concurrent("Testing PoetSerivce", async() => {
+  describe("Testing getALL()", async () => {
+    const poets = [
+      {
+        "id": "e7749f21-9cf9-4981-b7a8-2ce262f159f6",
+        "name": "عنترة بن شداد",
+        "time_period": "جاهلي",
+      },
+      {
+      "id": "e7749f21-9cf9-4981-b7a8-2ce262f159f6",
+      "name": "عنترة بن شداد",
+      "time_period": "جاهلي",
+      }
+    ] as Poet[];
+    test("Gets data successfully from Database", async() => {
+      vi.spyOn(PoetDB, "getAll").mockResolvedValue(poets)
+
+      const result = await PoetService.getAll()
+      expect(result).toStrictEqual(poets)
+    })
+    test("Returns false if no data is not available", async() => {
+      vi.spyOn(PoetDB, "getAll").mockResolvedValue([])
+
+      const result = await PoetService.getAll()
+      expect(result).toStrictEqual(false)
+    })
+  })
+
   describe("Testing getOneWithLiterature()", async () => {
     const poetWithLiterature = {
         "id": "e7749f21-9cf9-4981-b7a8-2ce262f159f6",
@@ -74,6 +102,101 @@ describe.concurrent("Testing PoetSerivce", async() => {
       expect(result).toEqual(false)
     })
   
+  })
+
+  describe("Testing post()", async() => {
+    let name = "عنترة بن شداد",
+    time_period =  "جاهلي",
+    bio= "عنترة بن عمرو بن شداد بن معاوية بن قراد العبسي (525 م - 608 م) هو أحد أشهر شعراء العرب في فترة ما قبل الإسلام، اشتهر بشعر الفروسية، وله معلقة مشهورة. وهو أشهر فرسان العرب وأشعرهم وشاعر المعلقات والمعروف بشعره الجميل وغزله العفيف بعبلة.";
+
+    const poet = {name, time_period, bio} as Poet
+    test("Post data successfully after validation", async() => {
+      vi.spyOn(PoetDB, "post").mockResolvedValue(poet)
+
+      const result = await PoetService.post(poet);
+      expect(result).toStrictEqual(poet)
+    })
+    test("Return false if data validation failed", async() => {
+      vi.spyOn(PoetDB, "post").mockResolvedValue(poet)
+
+      const result1 = await PoetService.post({name, time_period} as Poet)
+      expect(result1).toEqual(false)
+      const result2 = await PoetService.post({name,bio} as Poet)
+      expect(result2).toEqual(false)
+      const result3 = await PoetService.post({time_period, bio} as Poet)
+      expect(result3).toEqual(false)
+    })
+  })
+
+  describe("Testing postMany()", async() => {
+    let name = "عنترة بن شداد",
+    time_period =  "جاهلي",
+    bio= "عنترة بن عمرو بن شداد بن معاوية بن قراد العبسي (525 م - 608 م) هو أحد أشهر شعراء العرب في فترة ما قبل الإسلام، اشتهر بشعر الفروسية، وله معلقة مشهورة. وهو أشهر فرسان العرب وأشعرهم وشاعر المعلقات والمعروف بشعره الجميل وغزله العفيف بعبلة.";
+
+    const poet1 = {name, time_period, bio} as Poet
+    const poet2 = {name, time_period, bio} as Poet
+    const poet3 = {name, time_period, bio} as Poet
+    const poet4 = {name, time_period} as Poet
+    const poet5 = {name, bio} as Poet
+    const poet6 = {time_period, bio} as Poet
+    test("Post valid data successfully after validation and return inValid ones", async() => {
+      vi.spyOn(PoetDB, "postMany").mockResolvedValue([poet1, poet2, poet3])
+
+      const result= await PoetService.postMany([poet1,poet2,poet3,poet4,poet5, poet6]);
+      expect(result).toBeTruthy()
+      if(result != false) {
+        expect(result.newPoets).toStrictEqual([poet1, poet2, poet3])
+        expect(result.notValidPoets).toStrictEqual([poet4, poet5, poet6])
+      }
+    })
+  })
+
+  describe("Testing update()", async() => {
+    let name = "عنترة بن شداد",
+    time_period =  "جاهلي",
+    bio= "عنترة بن عمرو بن شداد بن معاوية بن قراد العبسي (525 م - 608 م) هو أحد أشهر شعراء العرب في فترة ما قبل الإسلام، اشتهر بشعر الفروسية، وله معلقة مشهورة. وهو أشهر فرسان العرب وأشعرهم وشاعر المعلقات والمعروف بشعره الجميل وغزله العفيف بعبلة.";
+
+    test("Update poet data successfully after successful validation",async () => {
+      vi.spyOn(PoetDB, "update").mockResolvedValue({affected: 1} as UpdateResult);      
+
+      const result1 = await PoetService.update("1",{name} as Poet)
+      expect(result1).toEqual(1)
+      const result2 = await PoetService.update("1",{time_period} as Poet)
+      expect(result2).toEqual(1)
+      const result3 = await PoetService.update("1",{bio} as Poet)
+      expect(result3).toEqual(1)
+    })
+    test("return false after invalid poetData",async () => {
+      vi.spyOn(PoetDB, "update").mockResolvedValue({affected: 1} as UpdateResult);      
+
+      const result1 = await PoetService.update("1",{name: "sa"} as Poet)
+      expect(result1).toEqual(false)
+      const result2 = await PoetService.update("1",{time_period: "عثماني " as TimePeriodType} as Poet)
+      expect(result2).toEqual(false)
+      const result3 = await PoetService.update("1",{bio: 'sds'} as Poet)
+      expect(result3).toEqual(false)
+    })
+    test("return false after non-existing id",async () => {
+      vi.spyOn(PoetDB, "update").mockResolvedValue({affected: 0} as UpdateResult);      
+
+      const result1 = await PoetService.update("1",{name: "sa"} as Poet)
+      expect(result1).toEqual(false)
+    })
+  })
+
+  describe("Testing remove()", async() => {
+    test("Successfully deletes poet", async() => {
+      vi.spyOn(PoetDB, "remove").mockResolvedValue({affected: 1} as UpdateResult);      
+
+      const result1 = await PoetService.remove("1")
+      expect(result1).toEqual(1)
+    })
+    test("return false for non-existing id", async() => {
+      vi.spyOn(PoetDB, "remove").mockResolvedValue({affected: 0} as UpdateResult);      
+
+      const result1 = await PoetService.remove("1")
+      expect(result1).toEqual(false)
+    })
   })
 })
 
