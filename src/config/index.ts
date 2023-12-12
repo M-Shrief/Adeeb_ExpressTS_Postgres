9746fc56b5f506ec121cab1e34e1b8291c2c03fe
M-Shrief <m.shrief.dev@proton.me>
2023-12-12 22:27:10 +0200

    Removed process.Exit

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,9 +14,8 @@ export const REDIS = process.env.REDIS;
 
 if (!process.env.JWT_PRIVATE_FILE) {
   logger.error("JWT Private key is not defined")
-  process.exit(1)
 }
-export const JWT_PRIVATE = fs.readFileSync(process.env.JWT_PRIVATE_FILE).toString().trim()
+export const JWT_PRIVATE = fs.readFileSync(process.env.JWT_PRIVATE_FILE!).toString().trim()
 
 export const {
   NODE_ENV,
