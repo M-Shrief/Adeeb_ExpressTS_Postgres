3c971de6c92c3881aa3a2d052f0ced7c2a5d2306
M-Shrief <m.shrief.dev@proton.me>
2024-01-22 18:57:36 +0200

    Used config module to get config vars, added Signup method to server struct to complete Signup() rpc implementation.

--- users-service/main.go
+++ users-service/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"time"
+	"users-service/config"
 	"users-service/pb"
+
+	"users-service/auth"
 
 	"google.golang.org/grpc"
 )
@@ -18,14 +24,23 @@ func (s *server) PingPong(ctx context.Context, req *pb.PingRequest) (*pb.PongRes
 	return &pb.PongResponse{Message: "Pong"}, nil
 }
 
-const (
-	port = ":8080"
-)
+func (s *server) Signup(ctx context.Context, req *pb.SignupRequest) (*pb.SignupResponse, error) {
+	user := &pb.User{Id: "sadsad", Name: req.GetName(), Phone: req.GetPhone()}
+	token, err := auth.CreateToken(
+		time.Hour,
+		user,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't create jwt token, Error: %v", err)
+	}
+	return &pb.SignupResponse{User: user, Token: token}, nil
+}
 
 func main() {
-	listener, err := net.Listen("tcp", port)
+	listener, err := net.Listen("tcp", config.PORT)
 	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		log.Printf("Failed to start server: %v", err)
+		os.Exit(1)
 	}
 
 	grpcServer := grpc.NewServer()
@@ -34,6 +49,7 @@ func main() {
 
 	log.Printf("Server starter at: %v", listener.Addr())
 	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("Failed to start: %v", err)
+		log.Printf("Failed to start: %v", err)
+		os.Exit(1)
 	}
 }
