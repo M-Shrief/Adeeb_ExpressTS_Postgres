8c4ee5fcc1969b4041fc2fe8b6b1e7bf0126d2e4
M-Shrief <m.shrief.dev@proton.me>
2024-01-22 18:56:09 +0200

    Created auth module, and added CreateToken() to create JWT with private keys.

---
+++ users-service/auth/token.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"users-service/config"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func CreateToken(ttl time.Duration, content interface{}) (string, error) {
+	privateKey, err := os.ReadFile(config.JWT_PRIVATE_FILE)
+	if err != nil {
+		return "", fmt.Errorf("couldn't read secret: %v", err)
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("parse private key error: %w", err)
+	}
+
+	now := time.Now().UTC()
+
+	claims := make(jwt.MapClaims)
+	claims["user"] = content            // Our custom data.
+	claims["exp"] = now.Add(ttl).Unix() // The expiration time after which the token must be disregarded.
+	claims["iat"] = now.Unix()          // The time at which the token was issued.
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, claims).SignedString(key)
+	if err != nil {
+		return "", fmt.Errorf("create token error: %w", err)
+	}
+
+	return token, nil
+}
