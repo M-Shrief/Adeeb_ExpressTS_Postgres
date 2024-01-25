11af84c56b7d47209804dd8aa41d3ee3f03ab539
M-Shrief <m.shrief.dev@proton.me>
2024-01-25 16:02:30 +0200

    Implemented Login() rpc.

---
+++ users-service/services/login.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"time"
+	"users-service/auth"
+	"users-service/datasource"
+	"users-service/pb"
+)
+
+func (s *Server) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	existingUser, err := datasource.DB.GetUserByPhone(ctx, req.GetPhone())
+	if err != nil {
+		return nil, fmt.Errorf("user not found: %v", err)
+	}
+
+	err = auth.CompareHash(existingUser.Password, req.GetPassword())
+	if err != nil {
+		return nil, err
+	}
+
+	user := &pb.User{
+		Id:        datasource.ToString(existingUser.ID),
+		Name:      existingUser.Name,
+		Phone:     existingUser.Phone,
+		SignedFor: string(existingUser.SignedFor),
+	}
+
+	token, err := auth.CreateToken(time.Hour, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.LoginResponse{User: user, Token: token}, nil
+}
