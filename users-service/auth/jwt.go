573f9e2247c7c4648249e35cc26317053c9ad640
M-Shrief <m.shrief.dev@proton.me>
2024-01-28 08:43:45 +0200

    modified CreateJWT() parameters type.

--- users-service/auth/jwt.go
+++ users-service/auth/jwt.go
@@ -6,11 +6,12 @@ import (
 	"time"
 	"users-service/config"
 	"users-service/datasource"
+	"users-service/pb"
 
 	"github.com/golang-jwt/jwt"
 )
 
-func CreateJWT(ttl time.Duration, content interface{}, permissions []string) (string, error) {
+func CreateJWT(ttl time.Duration, user *pb.User, permissions []string) (string, error) {
 	privateKey, err := os.ReadFile(config.JWT_PRIVATE_FILE)
 	if err != nil {
 		return "", fmt.Errorf("couldn't read secret: %v", err)
@@ -24,7 +25,7 @@ func CreateJWT(ttl time.Duration, content interface{}, permissions []string) (st
 	now := time.Now().UTC()
 
 	claims := make(jwt.MapClaims)
-	claims["user"] = content            // Our custom data.
+	claims["user"] = user               // Our custom data.
 	claims["exp"] = now.Add(ttl).Unix() // The expiration time after which the token must be disregarded.
 	claims["iat"] = now.Unix()          // The time at which the token was issued.
 	claims["permissions"] = permissions
