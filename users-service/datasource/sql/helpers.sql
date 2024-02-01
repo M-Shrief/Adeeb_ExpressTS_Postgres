020b0a0be5ea949fa7b8dc4e840b47fd2dd8b8ef
M-Shrief <m.shrief.dev@proton.me>
2024-02-01 16:56:25 +0200

    Updated sql helpers for future improvements.

--- users-service/datasource/sql/helpers.sql
+++ users-service/datasource/sql/helpers.sql
@@ -1,19 +1,21 @@
 -- Some queries that we can use to extend service functionalities
 
-
--- name: NewServiceSignedFor :exec
-UPDATE users set signed_for = array_append(signed_for, $2) WHERE phone = $1;
-
 -- name: IsSignedFor :one
-SELECT id, phone, name, signed_for FROM users WHERE id = $1 AND $2 <@ signed_for;
--- SELECT id, phone, name, signed_for FROM users WHERE id = '6049b5c5-6166-4808-8d72-d818ea583459' AND '{"management"}'::"signed_for"[] <@ signed_for;
-
+SELECT id, phone, name, signed_for FROM users WHERE id = $1 AND $2::"signed_for"[] <@ signed_for;
 
 -- name: IsSigned :one
--- SELECT id, phone,
---   CASE
---     WHEN '{"management"}'<@ users."signed_for" THEN 'Yes'
---     ELSE 'No'
---   END 
---   AS is_signed
--- FROM users WHERE id = $1;
+SELECT id, phone,
+  CASE
+    WHEN '{"management"}'<@ users."signed_for" THEN 'Yes'
+    ELSE 'No'
+  END 
+  AS is_signed
+FROM users WHERE id = $1;
+
+-- name: CreateUserOrUpdateIfExists :one
+INSERT INTO users (name, phone, password, signed_for) 
+VALUES ($1, $2, $3, $4::"signed_for"[])
+ON CONFLICT("phone") 
+DO UPDATE SET 
+    signed_for = (select array_agg(distinct e) from unnest(array_append(users."signed_for", $5::"signed_for")) e)
+RETURNING *;
